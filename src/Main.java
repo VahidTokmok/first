1082b4b372b667c0be4162988432b35dd5bcf225
VahidTokmok <mirvakhid100197@yandex.ru>
2023-01-04 19:17:09 +0600

    first

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int nuber = 5;
+
+        switch (nuber){
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("tree");
+                break;
+            default:
+                System.out.println("null");
+                break;
+        }
+    }
+}
